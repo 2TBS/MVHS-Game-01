d7545db50a0d97a0e2f246f66b82235b8084d125
dbqeo <ben@fewdpew.me>
2016-10-07 22:02:03 -0700

    Basic main menu template
    For development on Saturday.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	///Opens credits menu. Author: NULL
+	void OpenCredits () {
+
+	}
+
+	///Opens the options menu. Author: NULL
+	void OpenOptions () {
+
+	}
+
+	///Opens the play/level select menu. Author: NULL
+	void OpenPlay () {
+		
+	}
+	
+	///Quits the game. Author: NULL
+	void Quit () {
+		
+	}
+
+	
+}
