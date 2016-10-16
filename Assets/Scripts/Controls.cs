a51e3a9245e9338af722c75fdc967be4e5d36bda
dbqeo <ben@fewdpew.me>
2016-10-15 20:17:46 -0700

    Added inputManager, controls, options

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using UnityEngine.UI;
+using System.Collections;
+
+///Methods for Control changing menu, with integrated InputManager by Ben C. 
+public class Controls : MonoBehaviour {
+
+//List of editable controls
+public KeyCode Forward; //1
+public KeyCode Back; //2
+public KeyCode Left; //3
+public KeyCode Right; //4
+public KeyCode Jump; //5
+public KeyCode Run; //6
+
+private KeyCode[] controlList;
+private Text[] textList;
+
+private String configPath; 
+string[] defaultControls = {"W","S","A","D","Space","LeftShift"};
+public Text infoText;
+
+Event currentEvent;
+	// Use this for initialization
+	void Start () {
+		configPath = Application.dataPath + "/controls.cfg";
+		controlList = new KeyCode[] {Forward, Back, Left, Right, Jump, Run};
+		textList = new Text[] {
+		GameObject.Find("Forward").GetComponent<Text> (),
+		GameObject.Find("Back").GetComponent<Text> (),
+		GameObject.Find("Left").GetComponent<Text> (),
+		GameObject.Find("Right").GetComponent<Text> (),
+		GameObject.Find("Jump").GetComponent<Text> (),
+		GameObject.Find("Run").GetComponent<Text> ()
+		};
+		
+		infoText = infoText.GetComponent<Text> ();
+		infoText.enabled = false;
+		
+		
+			if(File.Exists(configPath) && ControlsValid())  {
+			Debug.Log("Successfully loaded controls file");
+			} else {
+			Debug.Log("Controls file is nonexistent or corrupted. Generating new one...");
+			using (var writer = new StreamWriter(File.Create(configPath))) {}
+			WriteDefaultControls();
+		
+		}	
+		ReloadControls();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnGUI () {
+	currentEvent = Event.current;
+	}
+	
+	public KeyCode GetKey (int id) {
+		string[] lines = File.ReadAllLines(configPath);
+		return (KeyCode)System.Enum.Parse(typeof(KeyCode), lines[id-1]);
+	}
+	
+	//input detection mode
+	public void SetKey (int id) {
+		
+			Text buttonText = textList[id-1];
+			Debug.Log("Selecting Key " + id);
+			infoText.enabled = true;
+			StartCoroutine(WaitForKey(id));
+	}
+	 IEnumerator WaitForKey (int id) {
+	
+			Text buttonText = textList[id-1];
+			while(true) {
+				if(currentEvent != null && (currentEvent.isKey || currentEvent.isMouse)) {
+				if (currentEvent.keyCode == KeyCode.Backspace) {
+					   buttonText.text = GetKey(id).ToString();
+					   infoText.enabled = false;
+					   Debug.Log("Key Selection cancelled");
+					  yield break;
+				 } else if(currentEvent.keyCode != KeyCode.Backspace && currentEvent.keyCode != KeyCode.None) {
+					 buttonText.text = currentEvent.keyCode.ToString();
+					 Debug.Log("Key Selection successful");
+					 string[] arrLine = File.ReadAllLines(configPath);
+     				arrLine[id - 1] = buttonText.text;
+     				File.WriteAllLines(configPath, arrLine);
+		 			Debug.Log("Set key " + id + " to " + buttonText.text);
+					infoText.enabled = false;
+					ReloadControls();
+					yield break;
+				 } else yield return null;
+			} else yield return null;
+			
+			
+			} 
+		}
+		
+	
+	public void WriteDefaultControls () {
+		Debug.Log("Writing default controls...");
+		File.WriteAllLines(configPath, defaultControls);
+		ReloadControls();
+	}
+	
+	public void ReloadControls () {
+		for(int i = 0; i < controlList.Length; i++) {
+			controlList[i] = GetKey(i+1);
+			textList[i].text = GetKey(i+1).ToString();
+		}
+		
+		Debug.Log("Successfully reloaded controls");
+	}
+	
+	private bool ControlsValid () {
+		try {
+			ReloadControls();
+			return true;
+		} catch {
+			return false;
+		}
+	}
+}
---
+++ Assets/Scripts/Pl_InputManager.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+
+/// <summary> Custom InputManager by dbqeo </summary>
+public class Pl_InputManager : MonoBehaviour {
+
+//List of editable controls
+[SerializeField]
+private KeyCode Forward; //1
+[SerializeField]
+private KeyCode Back; //2
+[SerializeField]
+private KeyCode Left; //3
+[SerializeField]
+private KeyCode Right; //4
+[SerializeField]
+private KeyCode Jump; //5
+[SerializeField]
+private KeyCode Run; //6
+
+private KeyCode[] controlList;
+private string[] controlListNames;
+private String configPath; 
+
+string[] defaultControls = {"W","S","A","D","Space","LeftShift"};
+
+string[] axes = {"Vertical","Horizontal"};
+
+Event currentEvent;
+	// Use this for initialization
+	void Start () {
+
+		configPath = Application.dataPath + "/controls.cfg";
+		controlListNames = new string[] {"Forward","Back","Left","Right","Jump","Run"};
+		controlList = new KeyCode[] {Forward, Back, Left, Right, Jump, Run};
+		
+			if(File.Exists(configPath) && ControlsValid())  {
+				ReloadControls();
+			} else {
+			Debug.Log("Controls file is nonexistent or corrupted. Generating new one...");
+			using (var writer = new StreamWriter(File.Create(configPath))) {}
+			WriteDefaultControls();
+			ReloadControls();
+		}	
+		
+	}
+
+	///<summary> Gets the value being outputted by a certain axis </summary>
+	public float GetAxis (String axis) {
+		if(axis == "Horizontal") {
+			if(Input.GetKey(Key("Forward"))) return 1;
+			else if (Input.GetKey(Key("Back"))) return -1;
+			else return 0;
+		} else if(axis == "Vertical") {
+			if(Input.GetKey(Key("Left"))) return 1;
+			else if (Input.GetKey(Key("Right"))) return -1;
+			else return 0;
+		} else throw new System.ArgumentException("Invalid axis name");
+	}
+
+	///<summary> Gets the keycode associated with custom key names </summary>
+	public KeyCode Key (String keyName) {
+		for(int i = 0; i < controlListNames.Length; i++) {
+			if(keyName == controlListNames[i]) return Key(i+1);
+		}
+		throw new System.ArgumentException("Invalid key name");
+	}
+
+	public KeyCode Key (int id) {
+		string[] lines = File.ReadAllLines(configPath);
+		return (KeyCode)System.Enum.Parse(typeof(KeyCode), lines[id-1]);
+	}
+	
+	public void ReloadControls () {
+		for(int i = 0; i < controlList.Length; i++) {
+			controlList[i] = Key(i+1);
+		}
+		
+		Debug.Log("Successfully reloaded controls");
+	}
+
+	private void WriteDefaultControls () {
+		Debug.Log("Writing default controls...");
+		File.WriteAllLines(configPath, defaultControls);
+		ReloadControls();
+	}
+	
+	private bool ControlsValid () {
+		try {
+			ReloadControls();
+			return true;
+		} catch {
+			return false;
+		}
+	}
+}
