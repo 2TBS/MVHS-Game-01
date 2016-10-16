36bfe20de7bb440141a6d96650b9633f63a03257
Gertyerteg <SpencerChang2001@Gmail.com>
2016-10-16 14:54:53 -0700

    Added to chapter
    Added a test chapter preview image and means to load the texture.

--- Assets/Scripts/ButtonEventController.cs
+++ Assets/Scripts/ButtonEventController.cs
@@ -6,6 +6,8 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.IO;
+
 public class ButtonEventController : MonoBehaviour
 {
 	/*chapterSelected holds the int value of the chapter that the player is choosing
@@ -14,12 +16,52 @@ public class ButtonEventController : MonoBehaviour
 	*/
 	public int chapterSelected;
 	public Text chapterText;
+
+    /// <summary>
+    /// Holds the chapter preview images that correspond to the selected chapter.
+    /// </summary>
+    public Texture2D[] chapterImage;
+
+    /// <summary>
+    /// Properties of the chapter images.
+    /// </summary>
+    public Vector2 imagePosition, imageSize;
+
+    /// <summary>
+    /// The maximum amount of chapters
+    /// </summary>
+    public int maxChapters;
+
+    /// <summary>
+    /// Performs initialization on the controller
+    /// </summary>
 	public void Start()
 	{
+        // for now lets assume we have 2 chapters
+        maxChapters = 2;
 		//chapterText = GetComponent<Text> ();
 		chapterSelected=1;
+
+        // initializes the list of images
+        chapterImage = new Texture2D[maxChapters];
+        for (int i = 0; i < chapterImage.Length; i++)
+        {
+            // loads the file(s) and converts to bytes
+            byte[] data = File.ReadAllBytes(@"Textures/Chapter" + i + ".png");
+
+            chapterImage[i] = new Texture2D(600, 480);
+            chapterImage[i].LoadImage(data);
+        }
+        
+
 		Debug.Log ("Testing:chapterSelected is " + chapterSelected);
 	}
+
+    /// <summary>
+    /// Takes in the name of the button that was pressed and decides what to do
+    /// with it.
+    /// </summary>
+    /// <param name="buttonPressed">Name of the button that was pressed.</param>
 	public void findClickedButton(string buttonPressed)
 	{
 		if (buttonPressed.Equals ("next")) 
@@ -40,6 +82,16 @@ public class ButtonEventController : MonoBehaviour
 		}
 		Debug.Log (buttonPressed + " was clicked and chapterSelected is " + chapterSelected);
 		chapterText.text="Chapter " + chapterSelected + ", the blah blah";
+        
 	}
+
+    /// <summary>
+    /// Returns the current chapter's corresponding image.
+    /// </summary>
+    /// <returns>The texture of the current chapter.</returns>
+    public Texture2D getCurrentChapterImage()
+    {
+        return chapterImage[chapterSelected];
+    }
 }
 
