02db82fe80a335719046c5a5c4788a461aa62868
Ben <Ben.Cuan@gmail.com>
2016-11-06 16:33:52 -0800

    Combined Pl_InputManager and Controls into one file
    With some bugfixes as well!

--- Assets/Scripts/Player/Pl_InputManager.cs
+++ Assets/Scripts/Player/Pl_InputManager.cs
@@ -1,27 +1,137 @@
 ﻿using UnityEngine;
 using System;
 using System.IO;
+using UnityEngine.UI;
+using System.Collections;
 
-/// <summary> Custom InputManager by dbqeo </summary>
-public class Pl_InputManager : Controls {
+///Methods for Control changing menu, with integrated InputManager by Ben C. 
+public class Pl_InputManager : MonoBehaviour {
 
-string[] defaultControls = {"W","S","A","D","Space","LeftShift"};
+//List of editable controls
+public KeyCode Left, Right, Jump, Run, Crouch;
 
+//Array containing the above keyCodes
+protected KeyCode[] controlList;
+
+//Array containing the text displays in controls assign menu
+protected Text[] textList;
+
+//Array containing the display names for above keyCodes
+protected string[] controlListNames;
+
+//Dynamic path to controls.cfg
+protected String configPath; 
+
+//Default assigned controls
+string[] defaultControls = {"A","D","Space","LeftShift", "S"};
 string[] axes = {"Vertical","Horizontal"};
+
+//Text that displays message when in controls assign mode
+public Text infoText;
+
 
 Event currentEvent;
 	// Use this for initialization
 	void Start () {
+		controlListNames = new string[] {"Left","Right","Jump","Run", "Crouch"};
+		configPath = Application.dataPath + "/controls.cfg";
+		controlList = new KeyCode[] {Left, Right, Jump, Run, Crouch};
+		
+		infoText = infoText.GetComponent<Text> ();
+		infoText.enabled = false;
+		
 		
 		if(File.Exists(configPath) && ControlsValid())  {
-			ReloadControls();
-		} else {
+			Debug.Log("Successfully loaded controls file");
+			} else {
 			Debug.Log("Controls file is nonexistent or corrupted. Generating new one...");
 			using (var writer = new StreamWriter(File.Create(configPath))) {}
 			WriteDefaultControls();
-			ReloadControls();
-		}	
 		
+		}	
+		ReloadControls();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnGUI () {
+	currentEvent = Event.current;
+	}
+	
+	///reads key from controls.cfg
+	public KeyCode GetKeyInternal (int id) {
+		string[] lines = File.ReadAllLines(configPath);
+		return (KeyCode)System.Enum.Parse(typeof(KeyCode), lines[id-1]);
+	}
+
+	///overload of GetKey, intended for external use
+	public bool GetKey (string id) {
+		return Input.GetKey(Key(id));
+	}
+	
+	///begins input detection mode
+	public void SetKey (int id) {
+		
+			Text buttonText = textList[id-1];
+			Debug.Log("Selecting Key " + id);
+			infoText.enabled = true;
+			StartCoroutine(WaitForKey(id));
+	}
+
+	///While key is being set: Waits for a valid input
+	 IEnumerator WaitForKey (int id) {
+	
+			Text buttonText = textList[id-1];
+			while(true) {
+				if(currentEvent != null && (currentEvent.isKey || currentEvent.isMouse)) {
+				if (currentEvent.keyCode == KeyCode.Backspace) {
+					   buttonText.text = GetKeyInternal(id).ToString();
+					   infoText.enabled = false;
+					   Debug.Log("Key Selection cancelled");
+					  yield break;
+				 } else if(currentEvent.keyCode != KeyCode.Backspace && currentEvent.keyCode != KeyCode.None) {
+					 buttonText.text = currentEvent.keyCode.ToString();
+					 Debug.Log("Key Selection successful");
+					 string[] arrLine = File.ReadAllLines(configPath);
+     				arrLine[id - 1] = buttonText.text;
+     				File.WriteAllLines(configPath, arrLine);
+		 			Debug.Log("Set key " + id + " to " + buttonText.text);
+					infoText.enabled = false;
+					ReloadControls();
+					yield break;
+				 } else yield return null;
+			} else yield return null;
+			
+			
+			} 
+		}
+		
+	
+	public void WriteDefaultControls () {
+		Debug.Log("Writing default controls...");
+		File.WriteAllLines(configPath, defaultControls);
+		ReloadControls();
+	}
+	
+	public void ReloadControls () {
+		for(int i = 0; i < controlList.Length; i++) {
+			controlList[i] = GetKeyInternal(i+1);
+			textList[i].text = GetKeyInternal(i+1).ToString();
+		}
+		
+		Debug.Log("Successfully reloaded controls");
+	}
+	
+	protected bool ControlsValid () {
+		try {
+			ReloadControls();
+			return true;
+		} catch {
+			return false;
+		}
 	}
 
 	///<summary> Gets the value being outputted by a certain axis </summary>
@@ -45,12 +155,12 @@ Event currentEvent;
 		throw new System.ArgumentException("Invalid key name");
 	}
 
+	///Raw key method that accepts an integer id
 	public KeyCode Key (int id) {
 		string[] lines = File.ReadAllLines(configPath);
 		return (KeyCode)System.Enum.Parse(typeof(KeyCode), lines[id-1]);
 	}
 
-	public bool GetKey (String keyName) {
-		return Input.GetKey(Key(keyName));
-	}
+	
+
 }
