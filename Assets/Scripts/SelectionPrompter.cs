903ccde80fa4a42729979cdfa6a6493a7d6e7995
sandman1544 <sanjeev@Sanjeevs-MBP.T-mobile.com>
2017-02-12 19:17:16 -0800

    Old stuff(Doesn't work yet, but forgot to commit)
    Old stuff(Doesn't work yet, but forgot to commit)

--- Assets/Scripts/SelectionPrompter.cs
+++ Assets/Scripts/SelectionPrompter.cs
@@ -2,21 +2,40 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class SelectionPrompter : MonoBehaviour {
+public class SelectionPrompter : MonoBehaviour 
+{
 //Create the 4 buttons
-
+	public Button buttonOne;
+	public Button buttonTwo;
+	public Button buttonThree;
+	public Button buttonFour;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+		
 		//Init 4 buttons
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	//Do nothing here
-	}
-
-	public void ShowButtons (string button1Text, string button2Text, string button3Text, string button4Text) {
+	public void ShowButtons (int numButtons)
+	{
+		if (numButtons == 4) 
+		{
+			buttonOne.gameObject.SetActive(true);
+			buttonTwo.gameObject.SetActive(true);
+			buttonThree.gameObject.SetActive(true);
+			buttonFour.gameObject.SetActive(true);
+		} 
+		else if (numButtons == 3) 
+		{
+			buttonOne.gameObject.SetActive(true);
+			buttonTwo.gameObject.SetActive(true);
+			buttonThree.gameObject.SetActive(true);
+		} 
+		else if (numButtons == 2) 
+		{
+			buttonOne.gameObject.SetActive(true);
+			buttonTwo.gameObject.SetActive(true);
+		}
 		//Enable the four buttons, and wait for a response
 	}
 
